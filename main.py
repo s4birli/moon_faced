0a3f457fbc6bcfbfa944dca53c92b92b16de5220
Mehmet Sabırlı <mhmtsabirli80@hotmail.com>
2020-08-23 18:26:15 +0100

    changed files?

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from getFrames import *
 from difference import *
-pathIn="D:\\Source Codes\\moon_faced/Video\\video.mp4"
+pathIn="D:\\Source Codes\\moon_faced\\Video\\video.mp4"
 pathOut="D:\\Source Codes\\moon_faced\\Frames\\"
-compare_frames(pathOut)
-# getImages(pathIn, pathOut)
+##getImages(pathIn, pathOut)
+##compare_frames(pathOut)
+
+
+print("//////// Welcome /////////")
+print()
+val = int(input("what? "))
+print()
+print(val)
