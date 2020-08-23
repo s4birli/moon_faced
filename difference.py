decfe105f6bbdb0b0ed07a1140101b7d9a18b2fd
Kunal Dhariwal <kunaldhariwal05@gmail.com>
2020-08-23 18:20:57 +0530

    Get % between Images
    Please add a loop to compare all the images present in a folder and then save images with less than 30% difference in another folder.

---
+++ difference.py
@@ -0,0 +1,8 @@
+from diffimg import diff
+
+im1 = "C:\\Users\\kunal\\Desktop\\Trial\\Frames\\Frame0.jpg"
+im2 = "C:\\Users\\kunal\\Desktop\\Trial\\Frames\\Frame1.jpg"
+
+diff = diff(im2, im1)
+
+print(diff)
